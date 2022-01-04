adc20cdd0b95c3816c4c6c2e26651bdb6a86af3d
ken888686@gmail.com <asas456789>
2022-01-04 22:26:10 +0800

    function

--- src/index.ts
+++ src/index.ts
@@ -1,68 +1,25 @@
-let myStr: string = "Aaron";
-console.log(myStr);
-
-let num: number = 123;
-let isBoolean: boolean = true;
-let n: null = null;
-let u: undefined = undefined;
-
-// 容易混淆的類型
-let test: any;
-test = 1;
-test = "A";
-test = false;
-test = undefined;
-
-let arr: string[] = ["a", "b"];
-let arr2: string[][] = [
-  ["a", "b"],
-  ["1", "2"],
-];
-
-let tuple: [string, number, boolean] = ["a", 1, false];
-let tuple2: [string, number][] = [
-  ["a", 1],
-  ["1", 2],
-];
-
-enum LiveStatus {
-  SUCCESS = 0,
-  FAIL = -1,
-  STREAMING = 1,
-}
-const streamStatus: LiveStatus = LiveStatus.SUCCESS;
-
-let aaa: number | string | boolean = "a";
-aaa = 1;
-aaa = "s";
-aaa = false;
-
-type A = number | string;
-let a1: A = "a";
-a1 = 1;
-// a1 = false;
-
-interface User {
-  name: string;
-  age: number;
+function func1(a: string, b: string) {
+  return a + b;
 }
 
-type Card = {
-  name: string;
-  desc: string;
-};
-
-interface Card2 {
-  name: string;
-  desc: string;
+function func2(a: string, b: string): number {
+  return (a + b).length;
 }
 
-interface Card2 {
-  age?: number;
+function func3(score: number, name: string, isPassed: boolean) {
+  return `${name} scored ${score} points and ${isPassed ? "passed" : "failed"}`;
 }
 
-const obj: Card2 = {
-  name: "a",
-  desc: "...",
-  age: 1,
-};
+function test(a: number) {
+  console.log(a);
+}
+
+function func4(name: string, age?: number) {
+  if (!age) {
+    return -1;
+  }
+
+  let a: number = age;
+  test(age);
+  return `${name} is ${age} years old`;
+}
