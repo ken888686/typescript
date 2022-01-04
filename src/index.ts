13d97e710dbadff37a6976fb1a85f3857eddc426
ken888686@gmail.com <asas456789>
2022-01-04 23:54:21 +0800

    class + interface

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,19 @@
-// public(default): 公開
-// private: 私有
-// protected: 受保護
-
-class Live {
-  roomName: string;
-  private id: string;
-  protected hostName: string;
-
-  constructor(roomName: string, id: string, hostName: string) {
-    console.log("建立直播中...");
-    this.roomName = roomName;
-    this.id = id;
-    this.hostName = hostName;
-  }
+interface CarProps {
+  name: string;
+  age: number;
+  start: () => void;
 }
 
-class CarLive extends Live {
-  constructor(roomName: string, id: string, hostName: string) {
-    super(roomName, id, hostName);
+class Car implements CarProps {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
   }
 
   start() {
-    console.log(`${super.roomName}直播開始`);
+    console.log(`${this.name} is starting`);
   }
 }
-
-const live = new Live("第1號", "000001", "Aaron");
-console.log("live:", live);
-
-const carLive = new CarLive("第2號", "000002", "Aaron's car");
-console.log("carLive:", carLive);
-
-class Live2 {
-  // private
-  #name: string;
-  constructor(name: string) {
-    this.#name = name;
-  }
-}
-
-const live2 = new Live2("第3號");
-console.log("live2:", live2);
-
-// private: 打包後還是看得到
-// #: 打包後不會出現，實現真正的private
