c72be162c681cc22fe320db8ccf9d0381fe693bb
ken888686@gmail.com <asas456789>
2022-01-04 22:27:21 +0800

    arrow function

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,2 @@
-function func1(a: string, b: string) {
-  return a + b;
-}
-
-function func2(a: string, b: string): number {
-  return (a + b).length;
-}
-
-function func3(score: number, name: string, isPassed: boolean) {
-  return `${name} scored ${score} points and ${isPassed ? "passed" : "failed"}`;
-}
-
-function test(a: number) {
-  console.log(a);
-}
-
-function func4(name: string, age?: number) {
-  if (!age) {
-    return -1;
-  }
-
-  let a: number = age;
-  test(age);
-  return `${name} is ${age} years old`;
-}
+const func1 = () => {};
+const func2 = () => 1;
