0046d52f0166d33b4ad709fc64f555c7c3ddb1dd
ken888686@gmail.com <asas456789>
2022-01-05 00:02:25 +0800

    Generics

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,18 @@
-interface CarProps {
-  name: string;
-  age: number;
-  start: () => void;
+function print<T>(data: T) {
+  console.log("data:", data);
 }
 
-class Car implements CarProps {
-  name: string;
-  age: number;
+print<string>("Aaron");
+print<number>(10);
+print<boolean>(true);
 
-  constructor(name: string, age: number) {
-    this.name = name;
-    this.age = age;
-  }
-
-  start() {
-    console.log(`${this.name} is starting`);
+class Print<T> {
+  data: T;
+  constructor(data: T) {
+    this.data = data;
   }
 }
+const p1 = new Print<number>(10);
+console.log("p1:", p1);
+const p2 = new Print<string>("Aaron");
+console.log("p2:", p2);
